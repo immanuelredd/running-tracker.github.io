0a89801b93b30787c130d11f2ef0ee5570afd377
immanuelredd <66283891+immanuelredd@users.noreply.github.com>
2020-08-16 15:29:16 +0100

    error

--- app/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function handleSubmmit(event) {
-    event.preventDefault();
-
-    const entry = Number(document.querySelector("#entry").value);
-    console.log(entry);
-}
-
-const form = document.querySelector('form').addEventListener('submit', handleSubmmit);
