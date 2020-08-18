09f47c5ae01738287f2eaa936d0749bf7763f35f
immanuelredd <66283891+immanuelredd@users.noreply.github.com>
2020-08-18 21:21:50 +0100

    delete errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,12 +57,3 @@ function handleSubmit(event) {
 }
 
 const form = document.querySelector('form').addEventListener('submit', handleSubmit);
-function handleSubmmit(event) {
-    event.preventDefault();
-
-    const entry = Number(document.querySelector("#entry").value);
-    console.log(entry);
-
-}
-
-const form = document.querySelector('form').addEventListener('submit', handleSubmmit);
